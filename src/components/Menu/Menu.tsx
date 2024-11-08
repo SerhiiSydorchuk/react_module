9e144c12ac0d38581813c915f4b163c5a8934df1
SerhiiSydorchuk <sydorchuks93@gmail.com>
2024-11-08 20:49:56 +0100

    hw8

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import styles
 
 const Menu = () => {
     return (
-        <ul>
+        <ul className={styles.menu}>
            <li><Link to={''}>Home Page</Link></li>
            <li><Link to={'users'}>Users Page</Link></li>
            <li><Link to={'comments'}>Comments Page</Link></li>
            <li><Link to={'posts'}>Posts Page</Link></li>
            <li><Link to={'show'}>Show Posts with Comments</Link></li>
+            <hr/>
         </ul>
     );
 };
