3e6c8af20dcce590c20a5898f3905105e16623c2
SerhiiSydorchuk <sydorchuks93@gmail.com>
2024-11-09 10:45:31 +0100

    hw8

---
+++ src/components/Comments/comment.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {IComment} from "../../Model/IComment";
+type commentProps={comment:IComment}
+const Comment:FC<commentProps> = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Comment;
